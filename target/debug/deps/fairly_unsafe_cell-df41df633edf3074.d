ba51d3d67632137bf0b7125f364a5eb4ab13f4ed
Sam Gwilym <sam@gwil.garden>
2025-04-09 11:20:56 +0200

    Initial commit

---
+++ target/debug/deps/fairly_unsafe_cell-df41df633edf3074.d
@@ -0,0 +1,10 @@
+/Users/gwil/Projects/fairly_unsafe_cell/target/debug/deps/libfairly_unsafe_cell-df41df633edf3074.rmeta: src/lib.rs src/unchecked.rs Cargo.toml
+
+/Users/gwil/Projects/fairly_unsafe_cell/target/debug/deps/fairly_unsafe_cell-df41df633edf3074.d: src/lib.rs src/unchecked.rs Cargo.toml
+
+src/lib.rs:
+src/unchecked.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
